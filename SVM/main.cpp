78da94e439e5c96c2af45e0a2a337392d2660bad
stjo1018 <stjo1018@hs-karlsruhe.de>
2012-10-31 18:16:59 +0100

    added simple 2D test case...but the results are still wired :-/

--- SVM/main.cpp
+++ SVM/main.cpp
@@ -101,12 +101,65 @@ int _tmain(int argc, _TCHAR* argv[])
 	*/
 
 	GestureSVM svm;
-	svm.train(extractor->getFeatureVector(inputClass1),1);
-	svm.train(extractor->getFeatureVector(inputClass1),2);
+
+	std::vector<std::vector<float>> featuresClass1;
+	std::vector<std::vector<float>> featuresClass2;
+	
+	std::vector<float> c1;
+	c1.push_back(0.1);
+	c1.push_back(2.2);
+	featuresClass1.push_back(c1);
+	c1.clear();
+	
+	c1.push_back(0.2);
+	c1.push_back(2.5);
+	featuresClass1.push_back(c1);
+	c1.clear();
+	
+	c1.push_back(0.15);
+	c1.push_back(2.3);
+	featuresClass1.push_back(c1);
+	c1.clear();
+	
+	//class 2
+	std::vector<float> c2;
+	c2.push_back(3.1);
+	c2.push_back(0.2);
+	featuresClass2.push_back(c2);
+	c2.clear();
+	
+	c2.push_back(3.2);
+	c2.push_back(0.5);
+	featuresClass2.push_back(c2);
+	c2.clear();
+	
+	c2.push_back(3.15);
+	c2.push_back(0.3);
+	featuresClass2.push_back(c2);
+	c2.clear();
+
+	//svm.train(extractor->getFeatureVector(inputClass1),1);
+	//svm.train(extractor->getFeatureVector(inputClass1),2);
+	
+	std::vector<std::vector<float>>::iterator iter;
+	
+	for(iter = featuresClass1.begin(); iter != featuresClass1.end();iter++)
+	{
+		svm.train(*iter,1);
+	}
+
+	for(iter = featuresClass2.begin(); iter != featuresClass2.end();iter++)
+	{
+		svm.train(*iter,2);
+	}
 
 	svm.generateModel();
 
-	double predictedClass = svm.predictGesture(extractor->getFeatureVector(toPredict));
+	std::vector<float> predict;
+	predict.push_back(1.5);
+	predict.push_back(3.2);
+
+	double predictedClass = svm.predictGesture(predict);
 	printf("Predicted as Class %f",predictedClass);
 	
 	getchar();

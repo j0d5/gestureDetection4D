53bd964e98fdedc865d6ce799f6287bdd051ada2
johannesw <wejo1017@hs-karlsruhe.de>
2012-11-14 14:19:49 +0100

    puhh..

--- GestureDetection4D/Detection.h
+++ GestureDetection4D/Detection.h
@@ -15,6 +15,8 @@
 bool g_IsTrainMode = false;
 int g_CurrentTrainClassID = -1;
 
+int g_predictedClass = -1;
+
 //global list for training
 list<XnPoint3D> g_pointList4Training(BUFFER_SIZE);
 
@@ -45,7 +47,13 @@ std::vector<float> extractFeatureVectorFromBuffer() {
 	std::vector<Point3D> pVector;
 
 	//detection Mode
-	if(!g_IsTrainMode) {
+	if(g_IsTrainMode) {
+		for(list<XnPoint3D>::iterator it = g_pointList4Training.begin(); it != g_pointList4Training.end(); ++it) {
+			pVector.push_back(convertPoint(&(*it)));
+		}
+		printf("Training Data Size: %d",pVector.size());
+	}else
+	{
 		if(!g_pointBuffer.isFull())
 		{
 			printf("Error: Buffer not full!!!\n");
@@ -64,13 +72,7 @@ std::vector<float> extractFeatureVectorFromBuffer() {
 	#endif
 
 	}
-	//training Mode
-	else {
-
-		for(list<XnPoint3D>::iterator it = g_pointList4Training.begin(); it != g_pointList4Training.end(); ++it) {
-			pVector.push_back(convertPoint(&(*it)));
-		}
-	}
+	
 
 	return g_featureExtractor.getFeatureVector(pVector);
 }
@@ -136,12 +138,9 @@ void doTraining()
 				g_PreGestureSVM.train(feature,1);
 				//train gesture svm
 				g_gestureSVM.train(feature, g_CurrentTrainClassID);
-				
-  
+				 
 			}
-		//replays the current ONI File in trainig mode. 
-		playFileSilent((*iter)->getFilepath());
-
+		
 		printf("Training class %d\n", g_CurrentTrainClassID);
 		printf("Extract feature Vector from buffer\n");
 		std::vector<float> feature  = extractFeatureVectorFromBuffer();
@@ -150,8 +149,9 @@ void doTraining()
 	//generate and save svm model after after training all oni files
 	g_gestureSVM.generateModel(); // this has to be done after collecting feature vectors
 	g_gestureSVM.saveModel(SVM_MODEL_FILE);
-			g_PreGestureSVM.generateModel();
-			g_PreGestureSVM.saveModel(SVM_PRE_MODEL_FILE);
+	g_PreGestureSVM.generateModel();
+	g_PreGestureSVM.saveModel(SVM_PRE_MODEL_FILE);
+
 }
 
 void doQuery()
@@ -159,9 +159,12 @@ void doQuery()
 		std::vector<float> feature  = extractFeatureVectorFromBuffer();
 		double isGesture = g_PreGestureSVM.predictGesture(feature);
 		printf("Pre-Predicted as: %f\n",isGesture);
+		g_predictedClass = (int) isGesture;
+		
 		if(isGesture > 0)
 		{
-			double predictedClass = g_gestureSVM.predictGesture(feature);
-			printf("Predicted as Class: %f\n",predictedClass);
+			g_predictedClass = g_gestureSVM.predictGesture(feature);
+			printf("Predicted as Class: %f\n",g_predictedClass);
+			
 		}	
 }

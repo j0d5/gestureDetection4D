6e6d06920f16c9c917d5567b168eb2d55ac78ac4
johannesw <wejo1017@hs-karlsruhe.de>
2012-11-19 16:15:58 +0100

    changed frequency

--- GestureDetection4D/Constants.h
+++ GestureDetection4D/Constants.h
@@ -3,7 +3,7 @@
 #define BUFFER_SIZE 100
 
 //use every n-th hand update
-#define FEATURE_VECTOR_FREQUENCY 3
+#define FEATURE_VECTOR_FREQUENCY 2
 #define WINDOW_NAME "GestureDetection4D"
 #define SVM_MODEL_FILE "../../SVM_Model.txt"
 #define SVM_PRE_MODEL_FILE "../../SVM_Pre_Model.txt"

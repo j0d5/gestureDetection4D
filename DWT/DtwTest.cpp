c660a50f4118a21bff7681c02f28aa52bcd0de43
johannesw <wejo1017@hs-karlsruhe.de>
2013-04-16 12:56:30 +0200

    Added dwt project

---
+++ DWT/DtwTest.cpp
@@ -0,0 +1,35 @@
+// DWT.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "dtw.h"
+#include <vector>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Vector DTW Test" << endl;
+	vector<Point> mainVec;
+	vector<Point> testVec;
+	Point p1(1, 2, 3);
+	Point p2(2, 3, 4);
+	Point p3(3, 2, 3);
+	Point p4(2, 2, 3);
+
+	mainVec.push_back(p1);
+	mainVec.push_back(p2);
+	testVec.push_back(p3);
+	testVec.push_back(p4);
+
+	VectorDTW dtw1(mainVec.size(), 0.3);
+
+	double dist = dtw1.fastdynamic(mainVec, testVec);
+		
+	cout << "Distance: " << dist << endl;
+			
+	system("PAUSE");
+	return 0;
+	return 0;
+}
+
